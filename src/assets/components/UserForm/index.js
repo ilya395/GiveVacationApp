025d9e163ffc629a0383f2492691760c8986566a
ilya395 <ilya395.efimenko@yandex.ru>
2020-12-07 23:07:34 +0300

    upd with vacations 1

--- src/assets/components/UserForm/index.js
+++ src/assets/components/UserForm/index.js
@@ -1,87 +1,91 @@
-import React, { useEffect, useState } from 'react';
-
-import { Form, Input, Button, Checkbox } from 'antd';
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
-};
+import React, { useEffect, useState, useContext } from 'react';
+import Firebase from '../../context/firebaseContext';
 
 const UserForm = (props) => {
 
-    // const onFinish = values => {
-    //     console.log('Success:', values);
-    // };
-    
-    // const onFinishFailed = errorInfo => {
-    //     console.log('Failed:', errorInfo);
-    // };
+  const { history } = props;
+  const { getVacationsRef, getUsersRef } = useContext(Firebase);
+  const [id, setId] = useState(props.match.params.id);
+  const [allUsers, setAllUsers] = useState([]);
+  const [thisUserId, setThisUserId] = useState(0);
+  const [thisUserName, setThisUserName] = useState('');
+  const [thisUserSurname, setThisUserSurname] = useState('');
+  const [thisUserLogin, setThisUserLogin] = useState('');
+  const [thisUserRole, setThisUserRole] = useState('');
+
+
+  useEffect(async () => {
+    await getUsersRef()
+      .once('value')
+      .then(response => response.val())
+      .then(res => {
+          setAllUsers(res);
+          return res;
+      })
+      .then(res => {
+        if (id !== 'new-user') {
+
+          const user = res.find(item => +item.id === +id);
+
+          setThisUserName(user.name);
+          setThisUserSurname(user.surname);
+          setThisUserLogin(user.login);
+          setThisUserRole(user.role);
+        }
+      })
+      .catch(e => console.log(e))
+      .finally(() => console.log('сходили за юзерами'));
+  }, [id]);
+
+  const handleName = (event) => {
+    setThisUserName(event.target.value);
+  }
+
+  const handleSurname = (event) => {
+    setThisUserSurname(event.target.value);
+  }
+
+  const handleLogin = (event) => {
+    setThisUserLogin(event.target.value);
+  }
+
+  const handleRole = (event) => {
+    setThisUserRole(event.target.value);
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault();
+  }
 
     return (
       <>
-        user form
+        <h2>user form</h2>
+        <div>
+          <form onSubmit={submitForm}>
+              <div>
+                  <label htmlFor="name">Имя</label>
+                  <input type="text" id="name" name="name" defaultValue={thisUserName} onChange={handleName} />
+              </div>
+              <div>
+                  <label htmlFor="surname">Фамилия</label>
+                  <input type="text" id="surname" name="surname" defaultValue={thisUserSurname} onChange={handleSurname} />
+              </div>
+              <div>
+                  <label htmlFor="login">Логин</label>
+                  <input type="text" id="login" name="login" defaultValue={thisUserLogin} onChange={handleLogin} />
+              </div>
+              <div>
+                  <label htmlFor="role">Статус(роль)</label>
+                  <input type="text" id="role" name="role" defaultValue={thisUserRole} onChange={handleRole} />
+              </div>
+              <div>
+                  <button>
+                      Отправить
+                  </button>
+              </div>
+          </form>
+        </div>
       </>
-        // <Form
-        //   {...layout}
-        //   name="basic"
-        //   onFinish={onFinish}
-        //   onFinishFailed={onFinishFailed}
-        //   form={form}
-        // >
-        //   <Form.Item
-        //     label="Имя"
-        //     name="name"
-        //     rules={[
-        //       {
-        //         required: true,
-        //         message: 'Имя',
-        //       },
-        //     ]}
-        //   >
-        //     <Input />
-        //   </Form.Item>
-    
-        //   <Form.Item
-        //     label="Фамилия"
-        //     name="surname"
-        //     rules={[
-        //       {
-        //         required: true,
-        //         message: 'Фамилия',
-        //       },
-        //     ]}
-        //   >
-        //     <Input />
-        //   </Form.Item>
-
-        //   <Form.Item
-        //     label="Логин"
-        //     name="login"
-        //     rules={[
-        //       {
-        //         required: true,
-        //         message: 'Логин',
-        //       },
-        //     ]}
-        //   >
-        //     <Input />
-        //   </Form.Item>
-    
-        //   <Form.Item {...tailLayout}>
-        //     <Button type="primary" htmlType="submit">
-        //       Отправить
-        //     </Button>
-        //   </Form.Item>
-        // </Form>
     );
 }
 
